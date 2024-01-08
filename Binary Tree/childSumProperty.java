0fafef70d2d569d5321750d90a8afe599051d5dd
Harshit <harshitagg0310@gmail.com>
2024-01-08 14:34:22 +0530

    08-01-2024 Binary Tree

---
+++ Binary Tree/childSumProperty.java
@@ -0,0 +1,19 @@
+boolean ans = true;
+public boolean childSumPropertyHelp(TreeNode root){
+    if(root == null) return true;
+
+    boolean leftcheck = childSumPropertyHelp(root.left);
+    boolean rightcheck = childSumPropertyHelp(root.right);
+
+    int sum = 0;
+    if(root.right != null)sum = root.right.val;
+    if(root.left != null)sum = sum + root.left.val;
+    if(root.val != sum && root.left!= null && root.right != null)ans = false;
+
+    return true;
+}
+
+public boolean childSumProperty(TreeNode root){
+    childSumPropertyHelp(root);
+    return ans;
+}
