130ae64787a5115b3742f2e53bc6246b938e33ae
Harshit <harshitagg0310@gmail.com>
2024-01-24 13:29:55 +0530

    24-01-2024 DP

--- DP/MCM.java
+++ DP/MCM.java
@@ -1,4 +1,29 @@
 package DP;
 
 public class MCM {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2 ,3, 5, 1};
+        int ans = MCMRecursion(arr, 0, arr.length-1);
+        System.out.println(ans);
+    }
+
+    public static int MCMRecursion(int[] arr,  int si, int ei){
+        if(si+1 == ei){
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        for(int k = si+1; k <= ei-1; k++){
+            int fp = MCMRecursion(arr, si, k);
+            int sp = MCMRecursion(arr, k, ei);
+
+            int sw = arr[si] * arr[k] * arr[ei];
+
+            int total = fp + sp + sw;
+            if(total < min){
+                min = total;
+            }
+        }
+        return min;
+    }
 }
