c26c8417ce6e731faf3afe68d04d6b05d592163e
Harshit <harshitagg0310@gmail.com>
2024-01-26 16:50:35 +0530

    24-01-2024 DP

--- DP/MCM.java
+++ DP/MCM.java
@@ -5,7 +5,8 @@ public class MCM {
     public static void main(String[] args) {
         int[] arr = {4, 2 ,3, 5, 1};
 //        int ans = MCMRecursion(arr, 0, arr.length-1);
-        int ans = MCMTD(arr, 0, arr.length-1, new int[arr.length][arr.length]);
+//        int ans = MCMTD(arr, 0, arr.length-1, new int[arr.length][arr.length]);
+        int ans = MCMBU(arr);
         System.out.println(ans);
     }
 
@@ -50,6 +51,39 @@ public class MCM {
         }
         dp[si][ei] = min;
         return min;
+    }
+
+    public static int MCMBU(int[] arr){
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+
+        for(int slide = 1; slide <= n-1; slide++){
+            for (int si = 0; si <= n-slide-1; si++){
+                int ei = si+slide;
+
+                if(si+1 == ei){
+                    dp[si][ei] = 0;
+                }
+                if(dp[si][ei] != 0){
+                    return dp[si][ei];
+                }
+                int min = Integer.MAX_VALUE;
+                for(int k = si+1; k <= ei-1; k++){
+                    int fp = dp[si][k];
+                    int sp = dp[k][ei];
+
+                    int sw = arr[si]* arr[k] * arr[ei];
+
+                    int total = fp+sp+sw;
+
+                    if(total < min){
+                        min = total;
+                    }
+                    dp[si][ei] = min;
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 
 }
