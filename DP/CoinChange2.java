13bd514d4f3a34172d6f98e5df3dd156fd12ad75
Harshit <harshitagg0310@gmail.com>
2024-02-01 23:22:31 +0530

    01-02-2024 DP

--- DP/CoinChange2.java
+++ DP/CoinChange2.java
@@ -28,7 +28,7 @@ public class CoinChange2 {
 
     }
 
-    public int change(int amount, int[] coins) {
+    public int changeTopDown(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
 
@@ -37,5 +37,32 @@ public class CoinChange2 {
             // we have to fill this -1 because our dp will be storing 0 as an answer so this thing can fail thus increasing the time complexity
         }
         return f(n-1, amount , coins, dp);
+    }
+
+
+    public int change(int target, int[] coins){
+        int n = coins.length;
+
+        int[][] dp = new int[n][target+1];
+
+        for(int i = 0; i <= target; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = 1 ;
+            }
+        }
+        // cell meaning : arr[1][2] : if only 2 (idx = 0, idx = 1) coins were used what is the no, of ways to make amount equals to 2
+
+        // direction of filling left to right and top to bottom ( as the cell dp[n-1][target] tells if all coins were consider what is the no. of ways to make that given amount)
+
+        for(int idx = 1; idx < n; idx++){
+            for(int amount = 0; amount <= target; amount++){
+                int notTaken = dp[idx-1][amount];
+                int taken = 0;
+                if(amount >= coins[idx])taken = dp[idx][amount-coins[idx]];
+
+                dp[idx][amount] = taken+notTaken;
+            }
+        }
+        return dp[n-1][target];
     }
 }
