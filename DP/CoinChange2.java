6937d09bb0591411e57c041fba51bfdfa17d0b20
Harshit <harshitagg0310@gmail.com>
2024-02-01 22:50:44 +0530

    01-02-2024 DP

---
+++ DP/CoinChange2.java
@@ -0,0 +1,41 @@
+package DP;
+import java.util.*;
+
+public class CoinChange2 {
+    public int f(int idx, int target, int[] coins, int[][] dp){
+        // base case
+        if(idx == 0){
+            return dp[idx][target] = (target%coins[0] == 0)? 1:0;
+            // this is done because if idx== 0 and coins array only elemetn is not able to completely divide this means that we have to return 0 as there is no way that it can be divided and 1 as it can be divided by that remaining element
+        }
+
+        // dp check
+        if(dp[idx][target] != -1){
+            return dp[idx][target];
+        }
+
+        // recursion calls
+        // as this question is of no. of ways we need to ask take and not take strategy
+        int notTaken = f(idx-1, target, coins, dp);
+        int taken = 0;
+        if(coins[idx] <= target){
+            // target is smaller than the element in the idx this means that there is no way the target can become 0 so just don't take that element
+            taken = f(idx, target-coins[idx], coins, dp);
+        }
+
+        // return
+        return dp[idx][target] = taken + notTaken;
+
+    }
+
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+
+        for(int[] subarr : dp){
+            Arrays.fill(subarr, -1);
+            // we have to fill this -1 because our dp will be storing 0 as an answer so this thing can fail thus increasing the time complexity
+        }
+        return f(n-1, amount , coins, dp);
+    }
+}
