8d50e044f8037a167dc366c14b9b5333bcd03a7e
Harshit Aggarwal <harshitagg0310@gmail.com>
2024-02-29 02:11:58 +0530

    28-feb-2024 Tree

--- BST/populatingTheRIghtPointer.java
+++ BST/populatingTheRIghtPointer.java
@@ -27,8 +27,11 @@ class Solution {
         Queue<Node> q = new ArrayDeque<>();
         q.add(root);
 
+        // this code has to be done iterative because otherwise hum jo ek subtree ka rightmost and doosre subtree ka ussi level par leftmost ko connect karna hi bhul jaenge
+        // issiliye issi code ko iterative hona bahut jaruri hai
         while(q.size() > 0){
             int size = q.size();
+            // rem ko pehle define issiliye kiya hai kyuki baadme iski jarurt padegi
             Node rem = null;
 
             while(size-- > 0) {
@@ -41,10 +44,10 @@ class Solution {
                 if(rem.right != null){
                     q.add(rem.right);
                 }
-
+                // ensure karega ki saare bonds perfect tareeke se ml rhe hai ya nahi
                 if(size > 0) rem.next = q.peek();
             }
-
+            // jaise hi ek level process ho jaeag vaise hi uss level ke last vaale me null daalne ka kaam ye vaka piece of code karega
             rem.next = null;
         }
         return root;
