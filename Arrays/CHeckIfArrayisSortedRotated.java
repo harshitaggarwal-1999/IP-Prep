41446b955e8b3c53183d618b9ebd0f2d7d543a7a
Harshit Aggarwal <harshitagg0310@gmail.com>
2024-04-21 16:51:47 +0530

    21-April-2024 Arrays

---
+++ Arrays/CHeckIfArrayisSortedRotated.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class CHeckIfArrayisSortedRotated {
+    public boolean check(int[] nums) {
+
+
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>nums[(i+1)%nums.length])
+            {
+                k++;
+            }
+            if(k>1)
+            {
+                return false;
+            }
+        }
+        return true;
+
+
+
+
+    }
+}
